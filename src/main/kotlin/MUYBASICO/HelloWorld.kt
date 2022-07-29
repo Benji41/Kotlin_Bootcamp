1fb8dbc16d21a70b1ff611004a1c5e7f50afd8f3
Noe Benjamin Reynoso <nreynoso@geniuscode.mx>
2022-07-28 20:30:13 -0700

    String of multiple lines and declaring variables with type inference

--- src/main/kotlin/MUYBASICO/HelloWorld.kt
+++ src/main/kotlin/MUYBASICO/HelloWorld.kt
@@ -2,6 +2,14 @@ package MUYBASICO
 
 fun main(){
     println("Hello world")
+    // declaring and initialization of a variable with type inference
     val numeroRaro = 41+1.8
     println(numeroRaro)
+    // declaring and initialization of a variable without type inference
+    //String which haves multiple lines
+    val myDescription : String = """
+        Nombre: NOE BENJAMIN REYNOSO
+        EDAD : 23
+        ESTUDIANDO: KOTLIN """
+    print(myDescription)
 }
